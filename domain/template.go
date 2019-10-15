44801ffb94cb0f78eecad73040d9a980d30e6e3b
Jan Garaj <jan.garaj@gmail.com>
2019-10-15 23:56:21 +0000

    Fixed 4.4 template model

--- domain/template.go
+++ domain/template.go
@@ -24,8 +24,8 @@ type DiscoveryRule struct {
 	Description     string           `xml:"description,omitempty" json:"decription,omitempty" yaml:"description,omitempty"`
 	ItemPrototypes  []ItemPrototype  `xml:"item_prototypes>item_prototype,omitempty" json:"item_prototypes,omitempty" yaml:"item_prototypes,omitempty"`
 	GraphPrototypes []GraphPrototype `xml:"graph_prototypes>graph_prototype,omitempty" json:"graph_prototypes,omitempty" yaml:"graph_prototypes,omitempty"`
-	Preprocessing   []Step           `xml:"preprocessing>step,omitempty" json:"preprocessing,omitempty" yaml:"preprocessing,omitempty"`
 	MasterItem      MasterItem       `xml:"master_item,omitempty" json:"master_item,omitempty" yaml:"master_item,omitempty"`
+	Preprocessing   []Step           `xml:"preprocessing>step,omitempty" json:"preprocessing,omitempty" yaml:"preprocessing,omitempty"`
 }
 
 // GItem representation
@@ -37,15 +37,16 @@ type GItem struct {
 // Graph representation
 type Graph struct {
 	Name       string      `xml:"name,omitempty" json:"name,omitempty" yaml:"name,omitempty"`
+        Type       string      `xml:"type,omitempty" json:"type,omitempty" yaml:"type,omitempty"`
 	GraphItems []GraphItem `xml:"graph_items>graph_item,omitempty" json:"graph_items,omitempty" yaml:"graph_items,omitempty"`
 }
 
 // GraphItem representation
 type GraphItem struct {
-	Color     string `xml:"color,omitempty" json:"color,omitempty" yaml:"color,omitempty"`
-	Item      GItem  `xml:"item,omitempty" json:"item,omitempty" yaml:"item,omitempty"`
 	Sortorder int    `xml:"sortorder,omitempty" json:"sortorder,omitempty" yaml:"sortorder,omitempty"`
 	Drawtype  string `xml:"drawtype,omitempty" json:"drawtype,omitempty" yaml:"drawtype,omitempty"`
+	Color     string `xml:"color,omitempty" json:"color,omitempty" yaml:"color,omitempty"`
+	Item      GItem  `xml:"item,omitempty" json:"item,omitempty" yaml:"item,omitempty"`
 }
 
 // GraphPrototype representation
@@ -66,17 +67,17 @@ type Item struct {
 	Key           string        `xml:"key,omitempty" json:"key,omitempty" yaml:"key,omitempty"`
 	Delay         string        `xml:"delay,omitempty" json:"delay,omitempty" yaml:"delay,omitempty"`
 	History       string        `xml:"history,omitempty" json:"history,omitempty" yaml:"history,omitempty"`
+	Trends        string        `xml:"trends,omitempty" json:"trends,omitempty" yaml:"trends,omitempty"`
 	ValueType     string        `xml:"value_type,omitempty" json:"value_type,omitempty" yaml:"value_type,omitempty"`
 	Units         string        `xml:"units,omitempty" json:"units,omitempty" yaml:"units,omitempty"`
 	Description   string        `xml:"description,omitempty" json:"decription,omitempty" yaml:"description,omitempty"`
 	Applications  []Application `xml:"applications>application,omitempty" json:"applications,omitempty" yaml:"applications,omitempty"`
+	ValueMap      ValueMap      `xml:"valuemap,omitempty" json:"valuemap,omitempty" yaml:"valuemap,omitempty"`
 	Preprocessing []Step        `xml:"preprocessing>step,omitempty" json:"preprocessing,omitempty" yaml:"preprocessing,omitempty"`
 	MasterItem    MasterItem    `xml:"master_item,omitempty" json:"master_item,omitempty" yaml:"master_item,omitempty"`
-	Trends        string        `xml:"trends,omitempty" json:"trends,omitempty" yaml:"trends,omitempty"`
 	URL           string        `xml:"url,omitempty" json:"url,omitempty" yaml:"url,omitempty"`
 	RetrieveMode  string        `xml:"retrieve_mode,omitempty" json:"retrieve_mode,omitempty" yaml:"retrieve_mode,omitempty"`
 	Triggers      []Trigger     `xml:"triggers>trigger,omitempty" json:"triggers,omitempty" yaml:"triggers,omitempty"`
-	ValueMap      ValueMap      `xml:"valuemap,omitempty" json:"valuemap,omitempty" yaml:"valuemap,omitempty"`
 }
 
 // ItemPrototype representation
@@ -128,8 +129,8 @@ type Screen struct {
 
 // ScreenItem representation
 type ScreenItem struct {
-	Resourcetype int      `xml:"resourcetype,omitempty" json:"resourcetype,omitempty" yaml:"resourcetype,omitempty"`
-	Style        int      `xml:"style,omitempty" json:"style,omitempty" yaml:"style,omitempty"`
+	Resourcetype int      `xml:"resourcetype" json:"resourcetype,omitempty" yaml:"resourcetype,omitempty"`
+	Style        int      `xml:"style" json:"style,omitempty" yaml:"style,omitempty"`
 	Resource     Resource `xml:"resource,omitempty" json:"resource,omitempty" yaml:"resource,omitempty"`
 	Width        int      `xml:"width,omitempty" json:"width,omitempty" yaml:"width,omitempty"`
 	Height       int      `xml:"height,omitempty" json:"height,omitempty" yaml:"height,omitempty"`
@@ -138,25 +139,25 @@ type ScreenItem struct {
 	Colspan      int      `xml:"colspan,omitempty" json:"colspan,omitempty" yaml:"colspan,omitempty"`
 	Rowspan      int      `xml:"rowspan,omitempty" json:"rowspan,omitempty" yaml:"rowspan,omitempty"`
 	Elements     int      `xml:"elements,omitempty" json:"elements,omitempty" yaml:"elements,omitempty"`
-	Valign       int      `xml:"valign,omitempty" json:"valign,omitempty" yaml:"valign,omitempty"`
-	Halign       int      `xml:"halign,omitempty" json:"halign,omitempty" yaml:"halign,omitempty"`
-	Dynamic      int      `xml:"dynamic,omitempty" json:"dynamic,omitempty" yaml:"dynamic,omitempty"`
-	SortTriggers int      `xml:"sort_triggers,omitempty" json:"sort_triggers,omitempty" yaml:"sort_triggers,omitempty"`
-	URL          string   `xml:"url,omitempty" json:"url,omitempty" yaml:"url,omitempty"`
-	Application  string   `xml:"application,omitempty" json:"application,omitempty" yaml:"application,omitempty"`
+	Valign       int      `xml:"valign" json:"valign,omitempty" yaml:"valign,omitempty"`
+	Halign       int      `xml:"halign" json:"halign,omitempty" yaml:"halign,omitempty"`
+	Dynamic      int      `xml:"dynamic" json:"dynamic,omitempty" yaml:"dynamic,omitempty"`
+	SortTriggers int      `xml:"sort_triggers" json:"sort_triggers,omitempty" yaml:"sort_triggers,omitempty"`
+	URL          string   `xml:"url" json:"url,omitempty" yaml:"url,omitempty"`
+	Application  string   `xml:"application" json:"application,omitempty" yaml:"application,omitempty"`
 	MaxColumns   int      `xml:"max_columns,omitempty" json:"max_columns,omitempty" yaml:"max_columns,omitempty"`
 }
 
 // Step representation
 type Step struct {
 	Type   string `xml:"type,omitempty" json:"type,omitempty" yaml:"type,omitempty"`
-	Params string `xml:"params,omitempty" json:"params,omitempty" yaml:"params,omitempty"`
+	Params string `xml:"params" json:"params,omitempty" yaml:"params,omitempty"`
 }
 
 // Template representation
 type Template struct {
-	Name           string          `xml:"name,omitempty" json:"name,omitempty" yaml:"name,omitempty"`
 	Template       string          `xml:"template,omitempty" json:"template,omitempty" yaml:"template,omitempty"`
+	Name           string          `xml:"name,omitempty" json:"name,omitempty" yaml:"name,omitempty"`
 	Description    string          `xml:"description,omitempty" json:"description,omitempty" yaml:"description,omitempty"`
 	Groups         []Group         `xml:"groups>group,omitempty" json:"groups,omitempty" yaml:"groups,omitempty"`
 	Applications   []Application   `xml:"applications>application,omitempty" json:"applications,omitempty" yaml:"applications,omitempty"`
@@ -169,12 +170,12 @@ type Template struct {
 // Trigger representation
 type Trigger struct {
 	Expression   string       `xml:"expression,omitempty" json:"expression,omitempty" yaml:"expression,omitempty"`
+	RecoveryMode string       `xml:"recovery_mode,omitempty" json:"recovery_mode,omitempty" yaml:"recovery_mode,omitempty"`
 	Name         string       `xml:"name,omitempty" json:"name,omitempty" yaml:"name,omitempty"`
 	Priority     string       `xml:"priority,omitempty" json:"priority,omitempty" yaml:"priority,omitempty"`
 	Description  string       `xml:"description,omitempty" json:"description,omitempty" yaml:"description,omitempty"`
 	ManualClose  string       `xml:"manual_close,omitempty" json:"manual_close,omitempty" yaml:"manual_close,omitempty"`
 	Dependencies []Dependency `xml:"dependencies>dependency,omitempty" json:"dependencies,omitempty" yaml:"dependencies,omitempty"`
-	RecoveryMode string       `xml:"recovery_mode,omitempty" json:"recovery_mode,omitempty" yaml:"recovery_mode,omitempty"`
 }
 
 // ValueMap representation
